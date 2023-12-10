dba1c6c29c44ee6f862f2e8a508c259a8f50c9cd
Yaroslav Agapov <108273756+bbabayaga0@users.noreply.github.com>
2023-12-10 21:24:23 +0300

    Add files via upload

---
+++ lab16/index.php
@@ -0,0 +1,44 @@
+<?php
+
+class Animal {
+    protected $food;
+    protected $location;
+
+    public function __construct($food, $location) {
+        $this->food = $food;
+        $this->location = $location;
+    }
+
+    public function makeNoise() {
+        echo "Лает в {$this->location}";
+    }
+
+    public function eat() {
+        echo "Ест {$this->food} в {$this->location}";
+    }
+
+    public function sleep() {
+        echo "Спит в {$this->location}";
+    }
+}
+
+class Dog extends Animal {
+    private $name;
+
+    public function __construct($name, $food, $location) {
+        parent::__construct($food, $location);
+        $this->name = $name;
+    }
+
+    public function bark() {
+        echo "Гавкает: {$this->name}";
+    }
+}
+
+$dog = new Dog("Шарик " ,"кости ", "дворе ");
+$dog->makeNoise();
+$dog->eat();
+$dog->sleep();
+$dog->bark()
+
+?>
